d4c60ac81080c68f5b2a133ab454bdfa92720a83
Olexandr <102588949+sanyok185@users.noreply.github.com>
2026-04-22 10:38:33 +0300

    Update send.js

--- telegram-form/api/send.js
+++ telegram-form/api/send.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 Повідомлення: ${city}
     `;
 
-    await fetch(`https://api.telegram.org/botТВОЙ_TOKEN/sendMessage`, {
+    await fetch(`https://api.telegram.org/8229454375:AAFDowxTraZ1hB7zikAC6CWWmjRv_kv-eds/sendMessage`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
